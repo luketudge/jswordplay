d65f522e5fa287d2b7185259433d1d65cc323cd3
luketudge <luketudge@gmail.com>
2020-08-10 09:38:15 +0200

    misc improvements (yeah, I know: very bad commit message)

---
+++ target_words.js
@@ -0,0 +1,7 @@
+// set of target words for texthighlight.js
+
+const target_words = new Set([
+  "logic",
+  "objectivity",
+  "truth",
+]);
--- texthighlight.js
+++ texthighlight.js
@@ -1,25 +1,22 @@
 // CSS selectors
+// for finding the input box and output field in the HTML
 
 var input_selector = "input[name='textinput']";
 var result_selector = "p[id='result']";
 
 
-// misc constants
-
-var problematic_words = new Set(["logic", "objectivity", "truth"]);
-
-
-// text manipulation functions
+// text manipulation function(s)
+// write new string processing functions here to try them out
 
 function identity(text){
   return text;
 }
 
-function highlight_problematic(text){
+function highlight_words(text){
   var text_out= "";
   var words = text.split(/\b/);
   for(var i = 0; i < words.length; i++) {
-    if(problematic_words.has(words[i])) {
+    if(target_words.has(words[i])) {  // from target_words.js
       text_out = text_out + `<span class="highlighted">${words[i]}</span>`;
     }
     else {
@@ -31,8 +28,9 @@ function highlight_problematic(text){
 
 
 // attach a function to the HTML text field
+// can choose a function from among those defined above
 
-var func_da_wunc = highlight_problematic;
+var func_da_wunc = highlight_words;
 
 $(input_selector).on("keyup", function () {
   var content = $(input_selector).val();
